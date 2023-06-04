1dbb484d3ae405dc07b9d7640b1deaaa112e55ab
Bartosz Bryniarski <brylka@jelenia.com>
2023-06-04 08:56:10 +0200

    pozycja myszki w oknie, poziomowanie w poziomie

--- 7_mouse.py
+++ 7_mouse.py
@@ -1,11 +1,13 @@
 import sys
+
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMainWindow, QLabel, QApplication
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.resize(500,300)
+        self.resize(600,600)
         self.label = QLabel("kliknij w okno")
         self.setCentralWidget(self.label)
 
@@ -13,6 +15,11 @@ class MainWindow(QMainWindow):
         x = e.position().x()
         y = e.position().y()
         self.label.setText(f"Kliknięto w okno w pozycji o współrzędnych ({x}, {y})")
+
+        if x <= 300:
+            self.label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        else:
+            self.label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
 
 app = QApplication(sys.argv)
 window = MainWindow()
